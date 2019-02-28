b34c28b7186e70980278c5e0c38bc5724b41c2fe
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-02-28 19:14:24 +0000

    New packages added
    New packages added

---
+++ fire-fighting-autonomous-intelligent-agents/src/utils/configuration/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * Fire Fighting
+ * - Autonomous and Intelligent Agents
+ *   in the Fight against Forest Fires
+ * 
+ * Agents and Distributed Artificial Intelligence
+ * 
+ * Integrated Master of Informatics and Computing
+ * Faculty of Engineering of University of Porto
+ * 
+ * Authors:
+ * @author Bernardo Jose Leite - up201404464@fe.up.pt
+ * @author Bruno Miguel Pinto - up201502960@fe.up.pt
+ * @author Ruben Andre Barreiro - up201808917@fe.up.pt
+ *
+ */
+
+// The package of Utils.Configuration.
+package utils.configuration;
