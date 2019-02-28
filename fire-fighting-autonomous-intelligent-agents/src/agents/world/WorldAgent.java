7ef91fac97bb8e122d5a23cd2ab2ed3d1e0a5e28
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-02-28 16:55:40 +0000

    Some minor changes
    Some minor changes

--- fire-fighting-autonomous-intelligent-agents/src/agents/world/WorldAgent.java
+++ fire-fighting-autonomous-intelligent-agents/src/agents/world/WorldAgent.java
@@ -1,4 +1,5 @@
 /**
+ * 
  * Fire Fighting - Autonomous Intelligent Agents
  * 
  * Agents and Distributed Artificial Intelligence
@@ -15,17 +16,119 @@
 
 package agents.world;
 
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import agents.firestation.FireStationAgent;
+import agents.vehicles.aircraft.AircraftAgent;
+import agents.vehicles.drone.DroneAgent;
+import agents.vehicles.firetruck.FireTruckAgent;
 import jade.core.Agent;
+import world.nature.Fire;
+import world.nature.WaterResource;
+import world.nature.utils.WeatherSeasonType;
+import world.nature.utils.WindType;
 
 /**
- * Class responsible for a World Agent.
+ * The class responsible for a World Agent.
  */
 public class WorldAgent extends Agent {
 
+	// Constants/Invariants:
+	
 	/**
 	 * The default UID of the World Agent.
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * The current type of the Weather Season from the set {Spring, Summer, Autumn and Winter}
+	 */
+	private static WeatherSeasonType seasonType;	
 	
+	/**
+	 * The current type of Wind from the set {Very Windy, Windy and No Wind}
+	 */
+	private static WindType windType;
+	
+	/**
+	 * The boolean value that keeps information that allows to know if can occur periodically,
+	 * in a rare way, droughts (extreme dry situations) - Can only occurs in Summer Season
+	 */
+	private static boolean droughtSituation;
+	
+	/**
+	 * The float value that keeps the probability value of a drought (extreme dry situation) happens,
+	 * if it's possible and allowed
+	 * - [0%, 0%] probability interval,
+	 *   of drought (extreme dry situation) happen in spring, autumn and winter seasons
+	 * - a random [m%, n%] probability interval, from the set [0%, 100%],
+	 *   of drought (extreme dry situation) happen in summer season
+	 */
+	private static float[] droughtSituationProbabilityInterval;
+	
+	
+	// Global Instance Variables:
+	
+	/**
+	 * The matrix of grid/map that represents all the positions of the world.
+	 */
+	private Object[][] worldMap;
+	
+	
+	// Fixed agents (without/no movement)
+	/**
+	 * The Fire Station Agent in the world.
+	 */
+	private FireStationAgent fireStationAgent;
+	
+	/**
+	 * The Water Resources in the world.
+	 */
+	private WaterResource[] waterResources;
+	
+	
+	// Mobile agents (with movement)
+	
+	/**
+	 * The Aircraft Agents presented in the world.
+	 */
+	private AircraftAgent[] aircraftAgents;
+	
+	/**
+	 * The Drone Agents presented in the world.
+	 */
+	private DroneAgent[] droneAgents;
+	
+	/**
+	 * The Fire Truck Agents presented in the world.
+	 */
+	private FireTruckAgent[] fireTruckAgents;
+	
+	
+	// Independent agents (without/no movement)
+	
+	/**
+	 * The current fires in the world.
+	 */
+	private ArrayList<Fire> fires;
+	
+	/*
+	 * The number of water resources in the world.
+	 */
+	private int numWaterResources;
+	
+	/*
+	 * The current number of aircrafts in the world.
+	 */
+	private  int currentNumAircrafts;
+	
+	/*
+	 * The current number of fires in the world.
+	 */
+	private int currentNumFires;
+	
+	private ActionListener actionListener;
+	
+	private WorldMetricsStats worldMetricsStats;
 }
