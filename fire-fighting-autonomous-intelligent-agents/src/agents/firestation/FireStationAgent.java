6b6a699335f622984f116e3f0352d86508f8eec4
Rúben André Barreiro <ruben.barreiro.92@gmail.com>
2019-02-28 16:56:09 +0000

    Class of Fire Station Agent added
    Class of Fire Station Agent added

---
+++ fire-fighting-autonomous-intelligent-agents/src/agents/firestation/FireStationAgent.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ * Fire Fighting
+ * - Autonomous and Intelligent Agents
+ *   in the Fight against Forest Fires
+ * 
+ * Agents and Distributed Artificial Intelligence
+ * 
+ * Integrated Master of Informatics and Computing
+ * Faculty of Engineering of University of Porto
+ * 
+ * Authors:
+ * @author Bernardo Jose Leite - up201404464@fe.up.pt
+ * @author Bruno Miguel Pinto - up201502960@fe.up.pt
+ * @author Ruben Andre Barreiro - up201808917@fe.up.pt
+ *
+ */
+
+package agents.firestation;
+
+import jade.core.Agent;
+
+/**
+ * The class responsible for the Fire Station Agent.
+ */
+public class FireStationAgent extends Agent {
+
+	/**
+	 * The default UID of the Fire Station Agent.
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
